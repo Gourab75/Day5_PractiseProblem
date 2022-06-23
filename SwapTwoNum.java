f87e5411f18481b0bfd1e70f6f08cffecbf0f89e
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-06-23 13:43:07 +0530

    swap num file add

---
+++ SwapTwoNum.java
@@ -0,0 +1,18 @@
+package javapackage;
+
+import java.util.Scanner;
+
+public class SwapTwoNum {
+	public static void main(String[] args) {
+		Scanner Input = new Scanner(System.in);
+		System.out.print("Enter The Value of A : ");
+		int A = Input.nextInt(); // 10
+		System.out.print("Enter The Value of B : ");
+		int B = Input.nextInt(); // 20
+		System.out.println("Before Swaping " + A + " " + B);
+		int temp = A; // temp = 10
+		A = B; // A = 20
+		B = temp; // 10
+		System.out.println("After Swaping " + A + " " + B);
+	}
+}
