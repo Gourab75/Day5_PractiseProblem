6dee2effe4380c430163a7337ce4903c727bbdc1
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-06-23 13:47:37 +0530

    largest num file add

---
+++ LargestNumber.java
@@ -0,0 +1,26 @@
+package javapackage;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+	public static void main(String[] args) {
+		Scanner Input = new Scanner(System.in);
+		System.out.println("Enter The Three Number : ");
+		int A = Input.nextInt();
+		int B = Input.nextInt();
+		int C = Input.nextInt();
+		if (A > B) {
+			if (A > C) {
+				System.out.println(+A + " is Greater ");
+			} else {
+				System.out.println(C + " is Grater");
+			}
+		} else {
+			if (B > C) {
+				System.out.println(+B + " is Greater ");
+			} else {
+				System.out.println(C + " is Grater");
+			}
+		}
+	}
+}
