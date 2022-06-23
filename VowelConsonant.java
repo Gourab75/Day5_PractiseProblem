cd602508077e08bc8f3dcc10f52f0b5f6bf48840
Gourab kumar Gupta <32832817+GrvSit@users.noreply.github.com>
2022-06-23 13:45:49 +0530

    vowel consonant file add

---
+++ VowelConsonant.java
@@ -0,0 +1,19 @@
+package javapackage;
+
+import java.util.Scanner;
+
+public class VowelConsonant {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a character :");
+		Scanner Input = new Scanner(System.in);
+		char ch = Input.next().charAt(0);
+
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+			System.out.println("Given character is an vowel");
+		} else {
+			System.out.println("Given character is a consonant");
+
+		}
+	}
+}
